78abb1f182a82deff46034c4ac9113448f7a62cd
Ben Michaeli <benm@codeoasis.com>
2020-06-13 14:14:37 +0300

    change api key

--- src/services/weather-service/index.js
+++ src/services/weather-service/index.js
@@ -5,24 +5,23 @@ const LOCATION_URL = 'locations/v1/cities';
 const CURRENT_CONDITION_URL = 'currentconditions/v1';
 const FORECASTS_URL = 'forecasts/v1/daily/5day';
 const API_KEY = 'apikey=F65ITMcva7VLGjfdqpDi1gJOQwOMOiXX';
-const API_KEY2 = 'apikey=6EOxAJZQpzrQ7BdioNyhX8XyFS0MxOok'
 
 export const autocomplete = query => {
-  return axios.get(`${BASE_URL}/${LOCATION_URL}/autocomplete?${API_KEY2}&q=${query}`)
+  return axios.get(`${BASE_URL}/${LOCATION_URL}/autocomplete?${API_KEY}&q=${query}`)
     .catch(error => error);
 }
 
 export const currentConditions = locationKey => {
-  return axios.get(`${BASE_URL}/${CURRENT_CONDITION_URL}/${locationKey}?${API_KEY2}`)
+  return axios.get(`${BASE_URL}/${CURRENT_CONDITION_URL}/${locationKey}?${API_KEY}`)
     .catch(error => error);
 }
 
 export const forecasts = locationKey => {
-  return axios.get(`${BASE_URL}/${FORECASTS_URL}/${locationKey}?${API_KEY2}`)
+  return axios.get(`${BASE_URL}/${FORECASTS_URL}/${locationKey}?${API_KEY}`)
     .catch(error => error);
 }
 
 export const geolocation = (lat, lon) => {
-  return axios.get(`${BASE_URL}/${LOCATION_URL}/geoposition/search?${API_KEY2}&q=${lat},${lon}`)
+  return axios.get(`${BASE_URL}/${LOCATION_URL}/geoposition/search?${API_KEY}&q=${lat},${lon}`)
     .catch(error => error);
 }
