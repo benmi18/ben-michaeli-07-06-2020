62be8d9758e745489861a105d068c9fb9eb493b9
Ben Michaeli <benm@codeoasis.com>
2020-06-13 11:07:28 +0300

    Updates

---
+++ precache-manifest.a2ece69ab306a530b9df3ca9800b41dc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25dc332d21ddebf3b00fd5df555c7da",
+    "url": "/ben-michaeli-07-06-2020/index.html"
+  },
+  {
+    "revision": "81307098fe5a2a309be1",
+    "url": "/ben-michaeli-07-06-2020/static/css/main.316d7ad7.chunk.css"
+  },
+  {
+    "revision": "f24eed2d0e8fc398b34e",
+    "url": "/ben-michaeli-07-06-2020/static/js/2.4294d164.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/ben-michaeli-07-06-2020/static/js/2.4294d164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81307098fe5a2a309be1",
+    "url": "/ben-michaeli-07-06-2020/static/js/main.c0d0a7eb.chunk.js"
+  },
+  {
+    "revision": "3cdc39c205367d46d661",
+    "url": "/ben-michaeli-07-06-2020/static/js/runtime-main.2dcb056a.js"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/ben-michaeli-07-06-2020/static/media/15.39ac499c.png"
+  },
+  {
+    "revision": "0cf9cad1347244da8a9e197998e0eff9",
+    "url": "/ben-michaeli-07-06-2020/static/media/16.0cf9cad1.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/ben-michaeli-07-06-2020/static/media/17.3c85da3e.png"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/ben-michaeli-07-06-2020/static/media/41.421c13f1.png"
+  }
+]);
